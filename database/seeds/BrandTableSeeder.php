065f6022e13cf7e35d9dc9f00057d7918e76e967
te <longtranf@gmail.com>
2017-01-30 21:13:51 +0700

    Make Seed

---
+++ database/seeds/BrandTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BrandTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('brands')->insert([
+            'name' => 'Van\'s',
+            'number_of_products' => 0,
+        ]);
+    }
+}
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Alice',
+            'email' => 'alice@example.com',
+            'password' => bcrypt('123456'),
+            'role' => 1,
+        ]);
+    }
+}
